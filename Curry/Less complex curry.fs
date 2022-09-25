6d219374df902c246a62841e327bbab9d1a378ce
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-09-25 11:20:23 -0300

    Adicionar arquivos de projeto.

---
+++ Curry/Less complex curry.fs
@@ -0,0 +1,10 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let lessComplexCurry = fun z -> fun y -> fun x -> x+y+z
+
+let a = lessComplexCurry 4
+let b = a 5
+let c = b 10
+
+
+printfn $"\nResults:\n A: {a}\n B: {b} \n C: {c}"
