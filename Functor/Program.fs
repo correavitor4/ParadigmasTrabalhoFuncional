e7337181285bbebcc4b5b3cbc2b2bed8ef9039ba
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-09-25 20:49:57 -0300

    Criando plotagem de gráfico

--- Functor/Program.fs
+++ Functor/Program.fs
@@ -1,23 +1,28 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Plotly.NET
+open System
 
-//let sum x = fun y -> x + y
+let sum x = fun y -> x + y
 let mult x = fun y -> x * y
 let div x = fun y -> x / y
-//let sub x = fun y -> x - y
+let sub x = fun y -> x - y
 let exp x = fun y -> pown x y
 
-let xValues = [0..10] 
+let xValues = [-10..10] 
 
 //Create expressions for operators
 // 3x³ - 4x² + x/2
-let expr1 x = (mult 3 (exp x 3)) + (mult 4 (exp x 2)) + (div x 2)
+let expr1 x = (sum (sub (mult 3 (exp x 3))  (mult 4 (exp x 2)))  (div x 2))
 // 2x² + 8x
-let expr2 x = (mult 2 (exp x 2)) + (mult x 8)
+let expr2 x = (sum (mult 2 (exp x 2))  (mult x 8))
 
 let fexpr1 = List.map expr1 xValues
 let fexpr2 = List.map expr2 xValues
 
-printfn $"{fexpr1}"
-printfn $"{fexpr2}"
+[
+    Chart.Line(xValues,fexpr1, Name="Expression 1 = 3x³ - 4x² + x/2")
+    Chart.Line(xValues,fexpr2, Name="Expression 2 = 2x² + 8x")
+]
+|> Chart.combine
+|> Chart.withTitle "Functor creates a expressions"
+|> Chart.show
 
