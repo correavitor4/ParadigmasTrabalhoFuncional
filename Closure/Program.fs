08a6bd90242f43c6373cfd24008f558bc35f30ab
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-09-25 19:52:41 -0300

    Creating closures

---
+++ Closure/Program.fs
@@ -0,0 +1,39 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let count (name:string) = 
+    printfn $"\n\nFunction Count() with name '{name}' is created"
+    let mutable x = 0
+    let mutable deepLevel =0
+    fun () -> 
+        x <- x + 1
+        printfn $"{name} is called {x} times"
+        fun () ->  
+            deepLevel <- deepLevel + 1
+            printfn $"Deep level of {name} was called {deepLevel} times"
+      
+//when cx is called with ()() the deep level function is called
+      
+let c1 = count("c1")
+c1()
+c1()
+c1()() 
+
+let c2 = count ("c2")
+c2()
+c2()
+c2()
+c2()
+c2()()
+c2()
+c2()()
+c2()
+c2()
+
+
+let c3 = count ("c3")
+c3()
+c3()
+c3()()
+c3()()
+c3()()
